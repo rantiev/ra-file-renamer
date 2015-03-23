ce7d45bddfe4a4a8c83ea4791bbe94eac1e9b4b8
Pavel Shlykov <rantie86@gmail.com>
2015-03-23 22:37:48 +0300

    Finish module.

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
-var renamer = require('../lib/index.js');
+var renamer = require('../lib/index.js'),
+	args = args = require('minimist')(process.argv.slice(2));
 
-renamer.rename();
+renamer.rename(args.path, args.old, args.new);
